17b0a1d5343deb1866ecbae74f1a1074869d129a
Jakub Wilk <jw@jakubwilk.pl>
2021-03-11 18:38:46 +0100

    Edit code rules and add basic users endpoint config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SModule } from './co/s/s.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-    imports: [],
+    imports: [SModule, UsersModule, OrdersModule],
     controllers: [],
     providers: [],
 })
