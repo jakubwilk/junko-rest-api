7a97c4259fca01e70bf16f64ce240027354c4c2d
Jakub Wilk <jw@jakubwilk.pl>
2021-03-11 18:43:24 +0100

    Remove wrong imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { SModule } from './co/s/s.module';
 import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-    imports: [SModule, UsersModule, OrdersModule],
+    imports: [UsersModule],
     controllers: [],
     providers: [],
 })
