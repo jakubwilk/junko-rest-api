9ba753bf9d057286975df2a58d7eb42cb0f5d4a4
Jakub Wilk <jw@jakubwilk.pl>
2021-03-14 18:07:02 +0100

    Edit mailer method

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,32 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectSendGrid, SendGridService } from '@ntegral/nestjs-sendgrid';
+
+type EmailData = {
+    to: string;
+    from: string;
+    subject: string;
+    text: string;
+    html: string;
+};
 
 @Injectable()
 export class MailerService {
     constructor(@InjectSendGrid() private readonly _client: SendGridService) {}
 
     async sendEmail(email: string) {
-        const msg = {
+        const msg: EmailData = {
             to: email,
             from: 'jw@jakubwilk.pl',
             subject: 'Autorization Code',
             text: 'Your authorization code to finish register',
-            template_id: 'd-4280aa7bae444c1abb71d548b9cb02d1',
-            dynamic_template_data: {
-                message: 'Witaj użytkowniku',
-                email: email,
-                link: 'https://www.google.com',
-            },
+            html: `
+                <p>Witaj, by dokończyć proces rejestracji proszę ustawić hasło na konta: <br />
+                    <strong>${email}</strong>
+                </p>
+                <a href="https://www.google.com" target="_blank">custom_url_with_token</a>
+            `,
         };
 
-        this._client
-            .send(msg)
-            .then(() => {
-                console.log('Email sent');
-            })
-            .catch((error) => {
-                console.log('Error');
-                console.log(error);
-            });
+        const message = await this._client.send(msg);
+
+        if (!message) {
+            throw new HttpException(
+                'Server Error during send message. Please contact with administrator of this website.',
+                500,
+            );
+        }
     }
 }
